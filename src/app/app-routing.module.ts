ce4088b4ef14c222d801db062dcd6bebefa1cfef
Higor Miguel Tamanho <higormiguel887@gmail.com>
2024-09-17 21:57:00 -0300

    C:/Users/SALA DE INFORMÁTICA/OneDrive/Documentos/ProjetosLOGP Higor/paginas_infinitas/script-13.sql

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './home/home.component';
+import { CadUsuarioComponent } from './cad-usuario/cad-usuario.component';
 
 const routes: Routes = [
   {path:'', redirectTo:"catalogo", pathMatch:'full'},
    {path:'catalogo',component:CatalogoComponent},
    {path:'login',component:LoginComponent},
-   {path:'home',component:HomeComponent}
+   {path:'home',component:HomeComponent},
+   {path:'cadUsuario',component:CadUsuarioComponent}
   ];
 
 @NgModule({
